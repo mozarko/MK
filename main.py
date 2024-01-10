c5e33221446c9acd5c9ee0b1f0652552703cc072
M <mozarko@yandex.ru>
2024-01-10 20:05:09 +0700

    10 01 2024

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ datas['password'] = password
 url = 'https://auctions.aleado.ru/auth/login.php'
 s = requests.Session()
 loging = s.post(url, data=datas)
-f = open('result.html', 'w+', encoding='utf-8')
-f.write(loging.text)
-f.close()
+with open('result.html', 'w+', encoding='utf-8') as file:
+    file.write(loging.text)
+with open('result.html', encoding='utf-8') as file:
+    content = file.readlines()
+for el in content:
+    if 'Логин' in el:
+        print(f'Вход не выполнен')
+    if '/auth/logout.php' in el:
+        print(f'Удачный вход')
+data_get = {
+    'mrk': 'TOYOTA'
+}
+urL_get = 'https://auctions.aleado.ru/auctions/?p=project/findlots&s&ld'
+response = requests.get(urL_get, data=data_get)
+with open('toyota.html', 'w+', encoding='utf-8') as file:
+    file.write(response.text)
